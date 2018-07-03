cdd760e43e51f3310a45a3dc0335818668e50bfd
Metas Kerdwat <oakkub@users.noreply.github.com>
2018-07-03 12:08:04 +0700

    Create html_string_diff.sh

---
+++ html_string_diff.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# You might need to use 'sudo' to run this file
+# And you might also need to run this command to allow read/write permission: 'chmod -R 777 html_string_diff.sh'
+
+printf "old branch: "
+read old_branch
+printf "new branch: "
+read new_branch
+
+save_path="Tune/build/Localization-Diff"
+res_path="Tune/src/main/res"
+
+# Create directory with full access if they are not already exist
+mkdir -p $save_path
+
+string_values=("" "-in" "-ja" "-ko" "-th" "-zh" "-zh-rTW")
+languages=("en" "in" "ja" "ko" "th" "zh" "zh-rTW")
+
+for ((i = 0; i < ${#string_values[@]}; i++)); do
+    string_value=${string_values[$i]}
+    language=${languages[$i]}
+
+    # outputs git diff as a temp file
+    git diff --color $old_branch $new_branch -- $res_path/values$string_value/strings.xml > $save_path/temp.strings.$language.diff
+
+    # outputs temp file as a html file
+    ansi-to-html -n -x $save_path/temp.strings.$language.diff > $save_path/strings.$language.html
+
+    # remove the temp file
+    rm $save_path/temp.strings.$language.diff
+done
+
+# Allow read/write permissions
+chmod -R 777 $save_path
+
+echo "Done! Check $save_path"
